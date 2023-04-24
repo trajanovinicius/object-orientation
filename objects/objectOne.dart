1a1f807e4b5dd845409a51302ec62bd093a13328
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2023-04-24 15:06:14 -0300

    finalized initial study of object-oriented programming

---
+++ objects/objectOne.dart
@@ -0,0 +1,16 @@
+void main() {
+  String nome = "Laranja";
+  double peso = 100.2;
+  String cor = "Verde";
+  String sabor = "Doce e cítrica";
+  int diasDeColheita = 40;
+  bool isMadura;
+
+  if (diasDeColheita >= 30) {
+    isMadura = true;
+  } else {
+    isMadura = false;
+  }
+}
+
+// um objeto pode ser manipulado e receber características.
