39b3e04ea91be3e86a6dc6b9e87ccc5741159a41
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2023-04-24 15:23:47 -0300

    understanding the basic concepts of functions

---
+++ functions/functionOne.dart
@@ -0,0 +1,12 @@
+//função main: tudo que escrever dentro o dart identifica como o que deve ser rodado.
+// a função main, nós que implementamos porém a função print() só utilizamos para ver algo no console.
+/*
+Um função é importante para automatizar processos, facilitar, repetir e ser fácil de 
+configurar. criamos funções para centralizar o código que vai ser repetido para não repetir código.
+se vamos fazer um código que vai aparecer em vários lugares, isolamos ele em uma função e usamos.
+DRY: Dont't Repeat Yourself - Evite Repetir códido!
+
+Modularização: quando falamos de Modularização falamos de isolar código em funções, códigos
+que se repetem.
+Usamos a modularização para tornar a programação mais produtiva e dar facilidade na manutenção.
+*/
